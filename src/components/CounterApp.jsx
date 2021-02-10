18888ef0996dc08ebf297bf5fcf13b6827f660a1
ZeusSura <Alejandrogzh@gmail.com>
2021-02-09 18:01:00 -0600

    first committ

---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+import { Button, Container, Box } from "@material-ui/core";
+const CounterApp = () => {
+
+const [state, setstate] = useState({
+    counter1:1,
+    counter2:2
+})
+  return (
+    <>
+      <Container align="center" mt="2rem">
+        <Box mt={4}>
+          <h1>Counter {state.counter1}</h1>
+          <hr />
+          <Button variant="contained" color="secondary"
+          onClick={()=>{
+              setstate({
+                  ...state,
+                  counter1:state.counter1+1
+              })
+          }}
+          >
+            +1
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default CounterApp;
