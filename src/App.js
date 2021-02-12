1c24391745d3024f1395679fc71d613d191a6b36
Omar Guzman <omar_guzman@pegaduro.com>
2021-02-11 20:29:06 -0600

    casa

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Container } from "@material-ui/core";
 import React from "react";
 // import CounterApp from "./components/useState/CounterApp";
 // import CounterWithCustomHook from './components/useState/CounterWithCustomHook'
-import SimpleForm from "./components/useEffect/SimpleForm";
+// import SimpleForm from "./components/useEffect/SimpleForm";
+import FormWithCustomHooks from './components/useEffect/FormWithCustomHooks'
 const App = () => {
   return (
     <>
       <Container fixed>
-        <SimpleForm />
+        <FormWithCustomHooks />
       </Container>
     </>
   );
